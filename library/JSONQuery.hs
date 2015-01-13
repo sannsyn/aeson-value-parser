2918fd4553ff5408b0c590febdaa00fd5d329a3f
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-01-13 04:52:23 +0300

    Init

---
+++ library/JSONQuery.hs
@@ -0,0 +1,39 @@
+module JSONQuery where
+
+import BasePrelude hiding (maybe, bool)
+import Data.Text (Text)
+import Data.Scientific (Scientific)
+import qualified Data.Aeson as Aeson
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector as Vector
+
+
+type JSON = 
+  Aeson.Value
+
+type Q =
+  Kleisli [] JSON
+
+run :: Q a -> JSON -> [a]
+run = 
+  runKleisli
+
+value :: Text -> Q JSON
+value name =
+  Kleisli $ \case
+    Aeson.Object m -> toList $ HashMap.lookup name m
+    _ -> []
+
+element :: Int -> Q JSON
+element index =
+  Kleisli $ \case
+    Aeson.Array v -> toList $ v Vector.!? index
+    _ -> []
+
+child :: Q JSON
+child =
+  Kleisli $ \case
+    Aeson.Object m -> HashMap.elems m
+    Aeson.Array v -> toList v
+    _ -> []
+
