4ec304fad8c93f227b36e7df182e8186fecebb7c
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-01-30 08:44:24 +0300

    Implement termination by failures of inner nodes

--- library/Aeson/ValueParser.hs
+++ library/Aeson/ValueParser.hs
@@ -20,18 +20,11 @@ module Aeson.ValueParser
   Object,
   field,
   oneOfFields,
-  fieldOr,
-  oneOfFieldsOr,
-  possibleField,
-  oneOfPossibleFields,
-  matchField,
   fieldMap,
   foldlFields,
   -- * Array parsers
   Array,
   element,
-  elementOr,
-  possibleElement,
   elementVector,
   foldlElements,
   foldrElements,
@@ -49,13 +42,19 @@ import qualified Data.Vector as Vector
 import qualified Aeson.ValueParser.Vector as Vector
 
 
+-- * Shared
+-------------------------
+
+type Parse ast = ReaderT ast (ExceptT Error.Error (Except Error.Error))
+
+
 -- * Value
 -------------------------
 
 -- |
 -- A JSON 'Aeson.Value' parser.
 newtype Value a =
-  Value (ReaderT Aeson.Value (Except Error.Error) a)
+  Value (Parse Aeson.Value a)
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadError Error.Error)
 
 instance MonadFail Value where
@@ -63,14 +62,14 @@ instance MonadFail Value where
 
 {-# INLINE run #-}
 run :: Value a -> Aeson.Value -> Either Error.Error a
-run (Value effect) = runExcept . runReaderT effect
+run (Value effect) = join . runExcept . runExceptT . runReaderT effect
 
 -- ** Definitions
 -------------------------
 
 {-# INLINE astMatcher #-}
 astMatcher :: (Aeson.Value -> Either Text a) -> Value a
-astMatcher matcher = Value $ ReaderT $ except . left Error.message . matcher
+astMatcher matcher = Value $ ReaderT $ either (throwE . Error.message) return . matcher
 
 {-# INLINE array #-}
 array :: Array a -> Value a
@@ -155,8 +154,8 @@ bool = astMatcher $ \ case
 {-# INLINE fromJSON #-}
 fromJSON :: Aeson.FromJSON a => Value a
 fromJSON = Value $ ReaderT $ Aeson.fromJSON >>> \ case
-  Aeson.Error m -> (except . Left) (fromString m)
-  Aeson.Success r -> pure r
+  Aeson.Success r -> return r
+  Aeson.Error m -> lift $ throwE $ fromString m
 
 
 -- * Object parsers
@@ -165,7 +164,7 @@ fromJSON = Value $ ReaderT $ Aeson.fromJSON >>> \ case
 -- |
 -- A JSON 'Aeson.Object' parser.
 newtype Object a =
-  Object (ReaderT (HashMap Text Aeson.Value) (Except Error.Error) a)
+  Object (Parse (HashMap Text Aeson.Value) a)
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadError Error.Error)
 
 instance MonadFail Object where
@@ -173,78 +172,31 @@ instance MonadFail Object where
 
 {-# INLINE field #-}
 field :: Text -> Value a -> Object a
-field key (Value effect) = Object $ ReaderT $ \ object -> except $ case HashMap.lookup key object of
-  Just value -> case runExcept (runReaderT effect value) of
-    Right parsedValue -> Right parsedValue
-    Left error -> Left (Error.named key error)
-  Nothing -> Left (Error.Error (pure key) "Object contains no such key")
+field name fieldParser = Object $ ReaderT $ \ object -> case HashMap.lookup name object of
+  Just value -> case run fieldParser value of
+    Right parsedValue -> return parsedValue
+    Left error -> lift $ throwE $ Error.named name error
+  Nothing -> throwE (Error.Error (pure name) "Object contains no field with this name")
 
 {-# INLINE oneOfFields #-}
 oneOfFields :: [Text] -> Value a -> Object a
 oneOfFields keys valueParser = asum (fmap (flip field valueParser) keys)
 
-{-# INLINE possibleFieldInChurch #-}
-possibleFieldInChurch :: b -> (a -> b) -> Text -> Value a -> Object b
-possibleFieldInChurch nothing just key fieldParser = Object $ ReaderT $ \ object -> except $ case HashMap.lookup key object of
-  Just value -> case run fieldParser value of
-    Right parsedValue -> Right (just parsedValue)
-    Left error -> Left (Error.named key error)
-  Nothing -> Right nothing
-
-{-| Applies the value parser to the existing field, propagating its errors accordingly.
-    Only if the field does not exist, it executes /Alternative branch/.
-    IOW, it won't execute /Alternative branch/ if /Field parser/ fails.
-    This is what distinguishes @fieldOr name parser@ from the seemingly same @mplus (field name parser)@ -}
-fieldOr :: Text {-^ Field name -} -> Value a {-^ Field parser -} -> Object a {-^ Alternative branch -} -> Object a
-fieldOr name fieldParser alt = join $ possibleFieldInChurch alt return name fieldParser
-
-{-| Same as `fieldOr`,
-    with the only difference that it enumerates the provided list of field names
-    parsing the first existing one. -}
-oneOfFieldsOr :: [Text] -> Value a -> Object a -> Object a
-oneOfFieldsOr names valueParser objectParser = foldr (\ name -> fieldOr name valueParser) objectParser names
-
-{-| Applies the value parser to the existing field, propagating its errors accordingly, but
-    unlike `field` it doesn't fail if the field does not exist.
-    The `Maybe` wrapper is there to cover such a case. -}
-{-# INLINE possibleField #-}
-possibleField :: Text -> Value a -> Object (Maybe a)
-possibleField = possibleFieldInChurch Nothing Just
-
-{-| Same as `possibleField`,
-    with the only difference that it enumerates the provided list of field names
-    parsing the first existing one. -}
-{-# INLINE oneOfPossibleFields #-}
-oneOfPossibleFields :: [Text] -> Value a -> Object (Maybe a)
-oneOfPossibleFields keys valueParser = oneOfFieldsOr keys (fmap Just valueParser) (pure Nothing)
-
-{-| Branch out the parsing scenario based on which fields are defined.
-    The value parser returns a continuation of the object parser,
-    thus allowing you to continue parsing the object
-    in the context of a parsed field.
-    
-    Fails if the list of provided cases is empty. -}
-{-# INLINE matchField #-}
-matchField :: [(Text, Value (Object a))] -> Object a
-matchField = \ cases -> foldr tryCaseOr (noneFound (HashSet.fromList (fmap fst cases))) cases where
-  tryCaseOr (name, fieldParser) alt = join $ possibleFieldInChurch alt id name fieldParser
-  noneFound nameSet = fail ("None of the following fields were found: " <> show (toList nameSet))
-
 {-# INLINE fieldMap #-}
 fieldMap :: Value a -> Object (HashMap Text a)
-fieldMap fieldParser = Object $ ReaderT $ except . HashMap.traverseWithKey mapping where
+fieldMap fieldParser = Object $ ReaderT $ HashMap.traverseWithKey mapping where
   mapping key ast = case run fieldParser ast of
     Right parsedField -> return parsedField
-    Left error -> Left (Error.named key error)
+    Left error -> lift (throwE (Error.named key error))
 
 {-# INLINE foldlFields #-}
 foldlFields :: (state -> Text -> field -> state) -> state -> Value field -> Object state
-foldlFields step state fieldParser = Object $ ReaderT $ \ object -> except $ HashMap.foldlWithKey' newStep (pure state) object where
-  newStep stateEither key fieldAst = case run fieldParser fieldAst of
+foldlFields step state fieldParser = Object $ ReaderT $ \ object -> HashMap.foldlWithKey' newStep (pure state) object where
+  newStep stateE key fieldAst = case run fieldParser fieldAst of
     Right !parsedField -> do
-      !state <- stateEither
+      !state <- stateE
       return $ step state key parsedField
-    Left error -> Left (Error.named key error)
+    Left error -> lift (throwE (Error.named key error))
 
 
 -- * Array parsers
@@ -253,7 +205,7 @@ foldlFields step state fieldParser = Object $ ReaderT $ \ object -> except $ Has
 -- |
 -- A JSON 'Aeson.Array' parser.
 newtype Array a =
-  Array (ReaderT (Vector Aeson.Value) (Except Error.Error) a)
+  Array (Parse (Vector Aeson.Value) a)
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadError Error.Error)
 
 instance MonadFail Array where
@@ -261,44 +213,28 @@ instance MonadFail Array where
 
 {-# INLINE element #-}
 element :: Int -> Value a -> Array a
-element index elementParser = Array $ ReaderT $ \ array -> except $ case array Vector.!? index of
+element index elementParser = Array $ ReaderT $ \ array -> case array Vector.!? index of
   Just element -> case run elementParser element of
-    Right result -> Right result
-    Left error -> Left (Error.indexed index error)
-  Nothing -> Left (Error.Error (pure (fromString (show index))) "Array contains no element by this index")
-
-{-# INLINE elementOr #-}
-elementOr :: Int -> Value a -> Array a -> Array a
-elementOr index elementParser alt = join $ Array $ ReaderT $ \ array -> except $ case array Vector.!? index of
-  Just element -> case run elementParser element of
-    Right result -> Right (return result)
-    Left error -> Left (Error.indexed index error)
-  Nothing -> Right alt
-
-{-# INLINE possibleElement #-}
-possibleElement :: Int -> Value a -> Array (Maybe a)
-possibleElement index elementParser = Array $ ReaderT $ \ array -> except $ case array Vector.!? index of
-  Just element -> case run elementParser element of
-    Right result -> Right (Just result)
-    Left error -> Left (Error.indexed index error)
-  Nothing -> Right Nothing
+    Right result -> return result
+    Left error -> lift $ throwE $ Error.indexed index error
+  Nothing -> throwE $ Error.Error (pure (fromString (show index))) "Array contains no element by this index"
 
 {-# INLINE elementVector #-}
 elementVector :: Value a -> Array (Vector a)
-elementVector elementParser = Array $ ReaderT $ \ arrayAst -> except $ flip Vector.imapM arrayAst $ \ index ast -> case run elementParser ast of
-  Right element -> Right element
-  Left error -> Left (Error.indexed index error)
+elementVector elementParser = Array $ ReaderT $ \ arrayAst -> flip Vector.imapM arrayAst $ \ index ast -> case run elementParser ast of
+  Right element -> return element
+  Left error -> lift $ throwE $ Error.indexed index error
 
 {-# INLINE foldlElements #-}
 foldlElements :: (state -> Int -> element -> state) -> state -> Value element -> Array state
-foldlElements step state elementParser = Array $ ReaderT $ except . Vector.ifoldM' newStep state where
+foldlElements step state elementParser = Array $ ReaderT $ Vector.ifoldM' newStep state where
   newStep state index ast = case run elementParser ast of
-    Right !element -> Right $ step state index element
-    Left error -> Left (Error.indexed index error)
+    Right element -> return $ step state index element
+    Left error -> lift $ throwE $ Error.indexed index error
 
 {-# INLINE foldrElements #-}
 foldrElements :: (Int -> element -> state -> state) -> state -> Value element -> Array state
-foldrElements step state elementParser = Array $ ReaderT $ except . Vector.ifoldrM newStep state where
+foldrElements step state elementParser = Array $ ReaderT $ Vector.ifoldrM newStep state where
   newStep index ast nextState = case run elementParser ast of
-    Right !element -> Right $ step index element nextState
-    Left error -> Left (Error.indexed index error)
+    Right element -> return $ step index element nextState
+    Left error -> lift $ throwE $ Error.indexed index error
